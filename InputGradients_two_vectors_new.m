be2254415cb9c20e0d4798a75a9f1470ec922672
tyukin <84316901+tyukin@users.noreply.github.com>
2022-11-23 10:28:43 +0000

    Delete InputGradients_two_vectors_new.m

--- InputGradients_two_vectors_new.m
+++
@@ -1,9 +0,0 @@
-function [gradients loss] = InputGradients_two_vectors_new(dlnet,dlX,Y_ref,NameOutput)
-
-Y_latent = forward(dlnet,dlX,'Output',NameOutput);
-
-loss = sum((Y_latent-Y_ref).*(Y_latent-Y_ref));
-
-gradients = dlgradient(loss,dlX);
-
-end
